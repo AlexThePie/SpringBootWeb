d069606b876fbeaab13990f328a7ef7e62f974ac
AlexThePie <112374973+AlexThePie@users.noreply.github.com>
2023-05-19 16:10:37 +0400

    Initial commit

---
+++ src/test/java/ru/diasoft/springbootweb/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diasoft.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
